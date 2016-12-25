6fd81150c4c02924dfb2597cb6a0a952e963dd70
Artemi Egorov <artemi.egorov@gmx.ch>
2016-12-25 16:23:25 +0100

    leaflet basic setup. tiles loading not yet working.

--- typo3conf/ext/transition_tools/Resources/Public/JavaScript/main.js
+++ typo3conf/ext/transition_tools/Resources/Public/JavaScript/main.js
@@ -5,3 +5,12 @@
 $(document).ready(function() {
     console.log('JS is working');
 });
+
+if ($('#mapid').length > 0) {
+    var mymap = L.map('mapid').setView([47.3773697, -8.3966319], 11);
+
+    L.tileLayer('http://a.tile.openstreetmap.org/${z}/${x}/${y}.png', {
+        attribution: 'bla',
+        maxZoom: 18
+    }).addTo(mymap);
+}

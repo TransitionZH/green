4c173b01d28c6201aa96799bad4138096b5c3d1a
lu40 <lucaobertuefer@mailbox.org>
2016-12-27 16:56:08 +0100

    make map full height/width

--- typo3conf/ext/transition_tools/Resources/Public/JavaScript/main.js
+++ typo3conf/ext/transition_tools/Resources/Public/JavaScript/main.js
@@ -2,17 +2,28 @@
  * Transition Tools - JavaScripts
  */
 
+function resizeMap() {
+	var computedHeight = $(window).height() - 100; // footer & navbar are both 50px in height
+	$('#initiatives-map').height(computedHeight);
+}
+
 $(document).ready(function() {
-    console.log('JS is working');
+	// Set the map's height on first pageload
+	resizeMap();
+
+	// And resize map also when changing the window size
+	$(window).resize(function() {
+		resizeMap();
+	});
 });
 
 
 /*
  Map
 */
-if ($('#mapid').length > 0) {
+if ($('#initiatives-map').length > 0) {
     /* create leaflet map object */
-    var mymap = L.map('mapid').setView([47.38, 8.60], 11);
+    var mymap = L.map('initiatives-map').setView([47.38, 8.60], 11);
 
     /* load tiles from open street map */
     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -25,7 +36,7 @@ if ($('#mapid').length > 0) {
     var marker = L.marker([47.38, 8.70], {icon: L.AwesomeMarkers.icon({icon: 'coffee',markerColor: 'blue'})}).addTo(mymap);
 
     /* add a random popup to the marker */
-    marker.bindPopup('<div class="media organization"><a class="media-left" href=""><img class="media-object" src="http://www.placehold.it/100x100" alt="Generic placeholder image"></a><div class="media-body" style="color:#000"><h4 class="media-heading">Beispielorganisation</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.<div class="clearfix"></div><div class="organization-links"><a href="#">Website</a></div></div></div>')
+    marker.bindPopup('<div class="media organization"><a class="media-left" href=""><img class="media-object" src="http://www.placehold.it/100x100" alt="Generic placeholder image"></a><div class="media-body" style="color:#000"><h4 class="media-heading">Beispielorganisation</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.<div class="clearfix"></div><div class="organization-links"><a href="#">Website</a></div></div></div>');
 
 
 }

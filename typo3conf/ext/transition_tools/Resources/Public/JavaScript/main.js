3989a1ff7bc3947b118540a0149d04d1245b8e85
lu40 <lucaobertuefer@mailbox.org>
2016-12-27 17:20:15 +0100

    tidy up map code

--- typo3conf/ext/transition_tools/Resources/Public/JavaScript/main.js
+++ typo3conf/ext/transition_tools/Resources/Public/JavaScript/main.js
@@ -2,6 +2,27 @@
  * Transition Tools - JavaScripts
  */
 
+ /* create leaflet map object */
+ var map = L.map('initiatives-map');
+
+/* set map view to zürich */
+ map.setView([47.38, 8.60], 11);
+
+ /* load tiles from open street map */
+ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	 attribution: 'Maps data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+	 maxZoom: 18
+ }).addTo(map);
+
+
+ /* create a random marker */
+ var marker = L.marker([47.38, 8.60], {icon: L.AwesomeMarkers.icon({icon: 'coffee',markerColor: 'red'})}).addTo(map);
+ var marker = L.marker([47.38, 8.70], {icon: L.AwesomeMarkers.icon({icon: 'coffee',markerColor: 'blue'})}).addTo(map);
+
+ /* add a random popup to the marker */
+ marker.bindPopup('<div class="media organization"><a class="media-left" href=""><img class="media-object" src="http://www.placehold.it/100x100" alt="Generic placeholder image"></a><div class="media-body" style="color:#000"><h4 class="media-heading">Beispielorganisation</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.<div class="clearfix"></div><div class="organization-links"><a href="#">Website</a></div></div></div>');
+
+
 function resizeMap() {
 	var computedHeight = $(window).height() - 100; // footer & navbar are both 50px in height
 	$('#initiatives-map').height(computedHeight);
@@ -10,33 +31,10 @@ function resizeMap() {
 $(document).ready(function() {
 	// Set the map's height on first pageload
 	resizeMap();
+	map.invalidateSize();
 
 	// And resize map also when changing the window size
 	$(window).resize(function() {
 		resizeMap();
 	});
 });
-
-
-/*
- Map
-*/
-if ($('#initiatives-map').length > 0) {
-    /* create leaflet map object */
-    var mymap = L.map('initiatives-map').setView([47.38, 8.60], 11);
-
-    /* load tiles from open street map */
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: 'Maps data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
-        maxZoom: 18
-    }).addTo(mymap);
-
-    /* create a random marker */
-    var marker = L.marker([47.38, 8.60], {icon: L.AwesomeMarkers.icon({icon: 'coffee',markerColor: 'red'})}).addTo(mymap);
-    var marker = L.marker([47.38, 8.70], {icon: L.AwesomeMarkers.icon({icon: 'coffee',markerColor: 'blue'})}).addTo(mymap);
-
-    /* add a random popup to the marker */
-    marker.bindPopup('<div class="media organization"><a class="media-left" href=""><img class="media-object" src="http://www.placehold.it/100x100" alt="Generic placeholder image"></a><div class="media-body" style="color:#000"><h4 class="media-heading">Beispielorganisation</h4>Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.<div class="clearfix"></div><div class="organization-links"><a href="#">Website</a></div></div></div>');
-
-
-}
